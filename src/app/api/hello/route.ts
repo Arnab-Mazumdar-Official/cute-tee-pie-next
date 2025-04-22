2e4534450a3d40348b4a2e914a5a02686e66deff
Arnab-Mazumdar-Official-2024 <arnabmazumdar@creatixia.com>
2025-04-22 11:56:08 +0530

    update

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  console.log("Arnab Debug");
+  
   return NextResponse.json({ message: "Dashboard API works!", data: [1, 2, 3] });
 }
