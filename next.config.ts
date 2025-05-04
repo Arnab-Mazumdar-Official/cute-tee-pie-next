085f5b7f69b905e177a04961b2b008669ac48202
ArnabMazumdar <arnabmazumdar9@gmail.com>
2025-05-04 12:21:15 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  /* other config options here */
+  env: {
+    IP_INFO_TOKEN: process.env.IP_INFO_TOKEN,
+    MONGODB_URI: process.env.MONGODB_URI,
+    MYAPP_AWS_ACCESS_KEY_ID: process.env.MYAPP_AWS_ACCESS_KEY_ID,
+    MYAPP_AWS_SECRET_ACCESS_KEY: process.env.MYAPP_AWS_SECRET_ACCESS_KEY,
+    MYAPP_AWS_REGION: process.env.MYAPP_AWS_REGION,
+    NOTIFICATION_EMAIL_USER: process.env.NOTIFICATION_EMAIL_USER,
+    NOTIFICATION_EMAIL_PASS: process.env.NOTIFICATION_EMAIL_PASS,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    SECRET_KEY: process.env.SECRET_KEY,
+  },
 };
-
 
 export default nextConfig;
